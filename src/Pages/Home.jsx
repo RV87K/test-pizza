835a1aeb4ac79c6e4a1ab71d00a07b28785e4d7b
Roman K <39156575+RV87K@users.noreply.github.com>
2023-10-16 11:25:51 +1000

    feat: add pagination service

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,14 +3,16 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination'
 
 export default function Home({ searchValue }) {
   const [items, setItems] = React.useState([]);
   const [isLoading, setSetIsLoading] = React.useState(true);
-  const [categoryId, setCategoryId] = React.useState(0);
+  const [categoryId, setCategory] = React.useState(0);
   const [sortType, setSortType] = React.useState({ name: 'популярности', sortProperty: 'rating' });
+  const [currentPage, setCurrentPage] = React.useState(1)
 
-  const skeleton = [...new Array(6)].map((_, i) => <Skeleton key={i} />);
+  const skeletons = [...new Array(6)].map((_, i) => <Skeleton key={i} />);
   const pizzas = items
     .filter((obj) => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
     .map((obj, i) => <PizzaBlock key={i} {...obj} />);
@@ -20,7 +22,7 @@ export default function Home({ searchValue }) {
     const sortBy = sortType.sortProperty.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     fetch(
-      `https://6508872656db83a34d9c788f.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://6508872656db83a34d9c788f.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}`,
     )
       .then((res) => res.json())
       .then((json) => {
@@ -28,15 +30,17 @@ export default function Home({ searchValue }) {
         setSetIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+  console.log(currentPage)
   return (
-    <>
+    <div className="container">
       <div className="content__top">
-        <Categories value={categoryId} onClickCategory={(i) => setCategoryId(i)} />
-        <Sort value={sortType} onChangeSort={(obj) => setSortType(obj)} />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategory(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">{isLoading ? skeleton : pizzas}</div>
-    </>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
+    </div>
   );
 }
