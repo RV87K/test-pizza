f4e35532bde3410c2102f670eeb6b9f4f247ac7f
Roman K <39156575+RV87K@users.noreply.github.com>
2023-10-28 13:18:31 +1000

    feat: import axios

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
